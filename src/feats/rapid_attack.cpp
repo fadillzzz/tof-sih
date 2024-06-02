89e4d254cf41323a21b36fd8c878ff3a57124379
Kemal Fadillah <fadillzzz@gmail.com>
2024-06-01 21:23:46 -0700

    Rapid attack improvement

--- src/feats/rapid_attack.cpp
+++ src/feats/rapid_attack.cpp
@@ -3,14 +3,21 @@
 #include "../hooks.hpp"
 #include "../logger/logger.hpp"
 #include "../memory_manager.hpp"
+#include "hotkey.hpp"
 
 namespace Feats {
     namespace RapidAttack {
         uint8_t *rapidAttackAddress = 0;
-        bool enabled = false;
+
+        Config::field<bool> enabled;
+        Config::field<bool> disableAnim;
+
         DWORD oldProtect;
 
         void init() {
+            enabled = Config::get<bool>(confEnabled, false);
+            disableAnim = Config::get<bool>(confDisableCharAnim, false);
+
             MemoryManager memory;
             const auto result = memory.PatternScan("0F 28 ? 74 ? F3 0F 10 4F ? F3 0F 5C 4C 24 ? 0F 2F D1", 1);
 
@@ -22,20 +29,61 @@ namespace Feats {
             rapidAttackAddress = (uint8_t *)result[0];
 
             VirtualProtect(rapidAttackAddress + 2, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
+
+            Hooks::registerHook(
+                "SendAttackTargNS.SendAttackTargNS_C.Received_NotifyBegin",
+                [](SDK::UObject *pObject, SDK::UFunction *pFunction, void *pParams) -> Hooks::ExecutionFlag {
+                    const auto character = Globals::getCharacter();
+
+                    if (character != nullptr) {
+                        const auto skillComponent = character->SkillComponent;
+
+                        if (skillComponent != nullptr) {
+                            const auto localAnimMontageInfo = skillComponent->LocalAnimMontageInfo;
+                            const auto animMontage = localAnimMontageInfo.AnimMontage;
+
+                            if (animMontage != nullptr) {
+                                if (*enabled && *disableAnim && animMontage->SlotAnimTracks.Num() > 0) {
+                                    animMontage->SlotAnimTracks.Remove(0);
+                                }
+
+                                if ((!*enabled || !*disableAnim) && animMontage->SlotAnimTracks.Num() == 0) {
+                                    animMontage->SlotAnimTracks.IncrementNum();
+                                }
+                            }
+                        }
+                    }
+
+                    return Hooks::CONTINUE_EXECUTION;
+                },
+                Hooks::POST);
         }
 
-        void tick() { return; }
+        void toggle(bool isEnabled) {
+            if (isEnabled) {
+                // movaps xmm0, xmm0
+                *(rapidAttackAddress + 2) = 0xC0;
+            } else {
+                // movaps xmm0, xmm2
+                *(rapidAttackAddress + 2) = 0xC2;
+            }
+        }
+
+        void tick() {
+            if (Feats::Hotkey::hotkeyPressed(confToggleEnabled)) {
+                *enabled = !*enabled;
+                toggle(*enabled);
+            }
+        }
 
         void menu() {
-            if (ImGui::Checkbox("Rapid Attack", &enabled)) {
-                if (enabled) {
-                    // movaps xmm0, xmm0
-                    *(rapidAttackAddress + 2) = 0xC0;
-                } else {
-                    // movaps xmm0, xmm2
-                    *(rapidAttackAddress + 2) = 0xC2;
-                }
+            if (ImGui::Checkbox("Rapid Attack (fast weapon animation)", &enabled)) {
+                toggle(*enabled);
             }
+
+            ImGui::Indent();
+            ImGui::Checkbox("Disable character animation", &disableAnim);
+            ImGui::Unindent();
         }
     } // namespace RapidAttack
 } // namespace Feats
--- src/feats/rapid_attack.hpp
+++ src/feats/rapid_attack.hpp
@@ -1,5 +1,10 @@
 namespace Feats {
     namespace RapidAttack {
+        const std::string confPrefix = "/feats/rapidAttack";
+        const std::string confEnabled = confPrefix + "/enabled";
+        const std::string confToggleEnabled = confPrefix + "/toggleEnabled";
+        const std::string confDisableCharAnim = confPrefix + "/disableCharAnim";
+
         void init();
         void tick();
         void menu();
