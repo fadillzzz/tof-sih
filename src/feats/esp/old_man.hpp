4d8014eb6193948544d8d359d1eee927a88384d1
Kemal Fadillah <fadillzzz@gmail.com>
2024-06-27 02:06:44 -0700

    Removes old man from ESP

--- src/feats/esp/old_man.hpp
+++
@@ -1,7 +0,0 @@
-namespace Feats {
-    namespace Esp {
-        namespace OldMan {
-            std::vector<std::shared_ptr<SDK::AActor>> getActors(SDK::UWorld *world);
-        }
-    } // namespace Esp
-} // namespace Feats
